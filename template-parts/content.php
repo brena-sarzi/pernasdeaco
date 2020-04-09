9a55c6258491850ab045450d68ed2de03fe0231b
Brena Sarzi <brenasarzi32@gmail.com>
2020-04-09 15:07:01 -0300

    continuaçao dos posts

--- template-parts/content.php
+++ template-parts/content.php
@@ -1,17 +1,37 @@
+<!-- POST -->
 <article class="cards">
+    
+    <!-- IMAGEM DO POST -->
     <?php the_post_thumbnail('medium') ?>
-    <div class="text-cards">
-    <?php the_category(''); ?>    
-    <h2><?php the_title(); ?></h2>
-   <p>Publicado em <?php echo get_the_date(); ?> por <?php
-        the_author_posts_link(); ?></p>
-    <p><?php the_tags( 'Tags:',','); ?></p>
-    <?php
-    $excerpt = get_the_excerpt();
- 
-    $excerpt = substr($excerpt, 0, 150);
-    $result = substr($excerpt, 0, strrpos($excerpt, ' '));
-    echo $result;?>
-    </div>
+        
+        <!-- DIVISAO DE TEXTO -->
+        <div class="text-cards">
+            
+            <!-- CATEGORIAS -->
+            <?php the_category(''); ?>    
+            
+            <!-- TITULO DO POST -->
+            <h2><?php the_title(); ?></h2>
+            
+            <!-- DIA DE PUBLICAÇÃO -->
+            <p>Publicado em <?php echo get_the_date(); ?> por <?php
+            the_author_posts_link(); ?></p>
+            
+            <!-- TAGS -->
+            <p><?php the_tags( 'Tags:',','); ?></p>
+            
+            <!-- LIMITE DE LINHAS DO POST -->
+            <?php
+            $excerpt = get_the_excerpt();
+
+            $excerpt = substr($excerpt, 0, 150);
+            $result = substr($excerpt, 0, strrpos($excerpt, ' '));
+            echo $result;?>
+            
+            <div class="icon-post">
+            <i class="fas fa-book-reader"><div class="leia-mais">Leia mais...</div></i>
+            </div>
+        
+        </div>
 </article>
 
