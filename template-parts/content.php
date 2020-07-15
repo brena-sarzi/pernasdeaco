9a1df01b9c583145818f69b7df3a30f7e7d6136b
Brena Sarzi <brenasarzi32@gmail.com>
2020-07-15 15:19:57 -0300

    title cards

--- template-parts/content.php
+++ template-parts/content.php
@@ -9,14 +9,14 @@
         <div class="text-cards">
             
             <!-- CATEGORIAS -->
-            <?php the_category(''); ?>  
-
-            <!-- TITULO DO POST -->
-            <a href="<?php the_permalink(); ?>"><h2><?php the_title(); ?></h2></a>
+            <?php the_category(''); ?> 
             
             <!-- DIA DE PUBLICAÇÃO -->
             <span style="font-size:12px !important; font-weight: bolder;"><p>Publicado em <?php echo get_the_date(); ?> por <?php
             the_author_posts_link(); ?></p></span>
+
+            <!-- TITULO DO POST -->
+            <a href="<?php the_permalink(); ?>"><h2><?php the_title(); ?></h2></a>
             
             <!-- LIMITE DE LINHAS DO POST -->
             <p class="abstract">
