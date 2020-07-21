d9e08e75749efe3fd244aa1c297321fc5a61648d
Brena Sarzi <brenasarzi32@gmail.com>
2020-07-21 14:04:35 -0300

    menu mobile bootstap

--- footer.php
+++ footer.php
@@ -36,6 +36,23 @@
         $('a.dropdown-toggle').attr('data-toggle', 'dropdown');
     });
 </script>
+<script>
+    $(document).ready(function () {
+
+    $('.first-button').on('click', function () {
+
+    $('.animated-icon1').toggleClass('open');
+    });
+    $('.second-button').on('click', function () {
+
+    $('.animated-icon2').toggleClass('open');
+    });
+    $('.third-button').on('click', function () {
+
+    $('.animated-icon3').toggleClass('open');
+    });
+    });
+</script>
 <?php wp_footer(); ?> 
 </body>
 </html>
--- header.php
+++ header.php
@@ -6,12 +6,9 @@
     <meta name="viewport" content="width=device-width">
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
     <link href="https://fonts.googleapis.com/css2?family=Manrope&display=swap" rel="stylesheet">
-    <script
-    src="https://code.jquery.com/jquery-3.5.1.min.js"
-    integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
-    crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script type="text/javascript" src="https://res.cloudinary.com/dxfq3iotg/raw/upload/v1565190285/Scripts/xzoom.min.js"></script>
-<link rel="stylesheet" type="text/css" href="https://res.cloudinary.com/dxfq3iotg/raw/upload/v1565190284/Scripts/xzoom.css" media="all" />
+    <link rel="stylesheet" type="text/css" href="https://res.cloudinary.com/dxfq3iotg/raw/upload/v1565190284/Scripts/xzoom.css" media="all" />
     <title>Pernas de Aço</title>
     <?php wp_head(); ?>
 </head>
@@ -20,13 +17,22 @@
 <header>
             
     <!-- MENU MOBILE RESPONSIVO -->
-        <div class="row">
-            <input type="checkbox" id="bt_menu">
-            <label for="bt_menu"><i class="fas fa-bars"></i></label> 
-            <nav class="menu-mobile col-12">  
-                <?php wp_nav_menu( array('theme_location' => 'my_main_menu') ); ?>
-            </nav>
+        <nav class="navbar navbar-dark red lighten-1 mb-4">
+
+        <a class="navbar-brand" href="#">Expandindo Mundos</a>
+
+        <button class="navbar-toggler second-button" type="button" data-toggle="collapse" data-target="#navbarSupportedContent23"
+            aria-controls="navbarSupportedContent23" aria-expanded="false" aria-label="Toggle navigation">
+            <div class="animated-icon2"><span></span><span></span><span></span><span></span></div>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent23">
+            <ul class="navbar-nav mr-auto">
+            
+            </ul>
         </div>
+
+        </nav>
             
     <!-- SLIDE RESPONSIVO -->
         <div class="row">
