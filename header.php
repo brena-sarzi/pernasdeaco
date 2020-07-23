7f17eb5ded52830d8e30e6164619d35002e750c1
Brena Sarzi <brenasarzi32@gmail.com>
2020-07-23 14:06:19 -0300

    mudanças com a identidade do site

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script type="text/javascript" src="https://res.cloudinary.com/dxfq3iotg/raw/upload/v1565190285/Scripts/xzoom.min.js"></script>
     <link rel="stylesheet" type="text/css" href="https://res.cloudinary.com/dxfq3iotg/raw/upload/v1565190284/Scripts/xzoom.css" media="all" />
-    <title>Pernas de Aço</title>
+    <title>Expandindo Mundos</title>
     <?php wp_head(); ?>
 </head>
     
