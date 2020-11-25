24fe15159bc3ae99e4669c53ee26b6841bdc1221
Brena Sarzi <brenasarzi32@gmail.com>
2020-11-25 15:46:01 -0300

    cheackout

--- functions.php
+++ functions.php
@@ -1,4 +1,11 @@
 <?php 
+/**
+ * Register Custom Navigation Walker
+ */
+function register_navwalker(){
+	require_once get_template_directory() . '/inc/class-wp-bootstrap-navwalker.php';
+}
+add_action( 'after_setup_theme', 'register_navwalker' );
 
 require get_template_directory() . '/inc/customizer.php';
 
