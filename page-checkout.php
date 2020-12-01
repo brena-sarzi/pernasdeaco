e23694700cc8f1e22e82ac219022f0bec607fb18
Brena Sarzi <brenasarzi32@gmail.com>
2020-12-01 12:32:11 -0300

    pagseguro

--- page-checkout.php
+++ page-checkout.php
@@ -1,14 +1,22 @@
 <?php get_header();?>
+
 	<div class="container">
-		<div class="row">
-			<?php the_content(); ?>
+		<div class="row" style="color:white;">
+
+		<?php while (have_posts()) : the_post(); ?>
+
+		<?php the_content(); ?>
+
+		<?php endwhile; ?>
+
 		</div>
 	</div>
 
-<script>
-$('#customer_details .col-1').removeClass('col-1').addClass('col-12').css({"display": "inline-block", "vertical-align": "top"});
-$('#customer_details .col-2').removeClass('col-2').addClass('col-12').css('display','inline-block');
-$('.woocommerce-MyAccount-content .col-1').removeClass('col-1').addClass('col-12');
-$('.woocommerce-MyAccount-content .col-2').removeClass('col-2').addClass('col-12');
-</script>
+	<script>
+		$('#customer_details .col-1').removeClass('col-1').addClass('col-12').css({"display": "inline-block", "vertical-align": "top"});
+		$('#customer_details .col-2').removeClass('col-2').addClass('col-12').css('display','inline-block');
+		$('.woocommerce-MyAccount-content .col-1').removeClass('col-1').addClass('col-12');
+		$('.woocommerce-MyAccount-content .col-2').removeClass('col-2').addClass('col-12');
+	</script>
+
 <?php get_footer(); ?>
